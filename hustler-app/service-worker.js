5bab7a2bb731771531a81f27608754683fab7648
simonevitiello-beep <simonevitiello@hustleproduction.it>
2026-09-03 20:47:18 +0200

    Update service-worker.js

--- hustler-app/service-worker.js
+++ hustler-app/service-worker.js
@@ -1,4 +1,4 @@
-const CACHE = "hustler-tracker-v2";
+const CACHE = "hustler-tracker-v3";
 const ASSETS = ["./", "./index.html", "./manifest.json", "./icon-192.png", "./icon-512.png"];
 
 self.addEventListener("install", (e) => {
