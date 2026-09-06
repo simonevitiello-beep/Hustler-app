4feb87cd9e9bdabed540f5bdff3c7dfd79d9efcd
simonevitiello-beep <simonevitiello@hustleproduction.it>
2026-09-06 13:28:09 +0200

    v11: alzate posteriori nel G5, calf a 90 gradi

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE = "hustler-tracker-v10";
+const CACHE = "hustler-tracker-v11";
 const ASSETS = ["./", "./index.html", "./manifest.json", "./icon-192.png", "./icon-512.png"];
 
 self.addEventListener("install", (e) => {
