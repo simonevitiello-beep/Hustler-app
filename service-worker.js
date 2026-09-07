1010710191d7f3b197c7c117bb88f34ac9ca913c
simonevitiello-beep <simonevitiello@hustleproduction.it>
2026-09-07 18:14:34 +0200

    v12: suggerimento carico e suoneria in background

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE = "hustler-tracker-v11";
+const CACHE = "hustler-tracker-v12";
 const ASSETS = ["./", "./index.html", "./manifest.json", "./icon-192.png", "./icon-512.png"];
 
 self.addEventListener("install", (e) => {
