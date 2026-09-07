6d223fc9891d6c0017ea3b4d16e5ac9ccd4b92ed
simonevitiello-beep <simonevitiello@hustleproduction.it>
2026-09-07 18:53:33 +0200

    v15: logica suggerimento carico corretta

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE = "hustler-tracker-v14";
+const CACHE = "hustler-tracker-v15";
 const ASSETS = ["./", "./index.html", "./manifest.json", "./icon-192.png", "./icon-512.png"];
 
 self.addEventListener("install", (e) => {
