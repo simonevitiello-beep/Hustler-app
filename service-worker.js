f09a830bf57b809426667ee0d8587fce3a23bf74
simonevitiello-beep <simonevitiello@hustleproduction.it>
2026-09-10 12:19:22 +0200

    Sostituzione esercizio: motivo, lista per muscolo, dati sull'esercizio davvero fatto

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE = "hustler-tracker-v19";
+const CACHE = "hustler-tracker-v20";
 const ASSETS = ["./", "./index.html", "./manifest.json", "./icon-192.png", "./icon-512.png"];
 
 self.addEventListener("install", (e) => {
