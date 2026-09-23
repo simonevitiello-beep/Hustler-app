636536fe2eee708b541b3f8fc6780baf426ae205
simonevitiello-beep <simonevitiello@hustleproduction.it>
2026-09-23 22:39:48 +0200

    v28: le sedute passate mostrano anche gli esercizi registrati prima di un cambio di scheda

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE = "hustler-tracker-v27";
+const CACHE = "hustler-tracker-v28";
 const ASSETS = ["./", "./index.html", "./manifest.json", "./icon-192.png", "./icon-512.png", "./icon-192-maskable.png", "./icon-512-maskable.png"];
 
 self.addEventListener("install", (e) => {
